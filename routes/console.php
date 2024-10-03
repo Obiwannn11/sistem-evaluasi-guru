f88724309bd0aa55887d86b8f353152b74568f24
muammar200 <amuammar01@gmail.com>
2024-10-03 16:10:40 +0800

    tes ci/cd

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+// Tes
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
