4450ac503cc89628d6f6e3b02cf39a780f128e59
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2024-12-07 14:37:20 +0800

    menambahkan view CRUD gurus

---
+++ resources/views/gurus/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container">
+    <h1>Tambah Guru</h1>
+    <form action="{{ route('gurus.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="nama" class="form-label">Nama</label>
+            <input type="text" name="nama" id="nama" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" name="email" id="email" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" id="password" class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+    </form>
+</div>
+@endsection
