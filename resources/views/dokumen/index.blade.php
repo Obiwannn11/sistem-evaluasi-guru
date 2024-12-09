bd5c4eb2bf2d76a865aad3c089a58f8a62a665a2
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2024-12-09 16:08:23 +0800

    memperbaiki isi view dokumen

--- resources/views/dokumen/index.blade.php
+++ resources/views/dokumen/index.blade.php
@@ -1,39 +1,32 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <h1>Daftar Dokumen</h1>
-    <a href="{{ route('dokumens.create') }}" class="btn btn-primary">Tambah Dokumen</a>
-    <table class="table mt-3">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Guru</th>
-                <th>Kriteria</th>
-                <th>File</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($dokumens as $dokumen)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $dokumen->guru->nama }}</td>
-                    <td>{{ $dokumen->kriteria->nama_kriteria }}</td>
-                    <td>
-                        <a href="{{ asset('storage/' . $dokumen->file_path) }}" target="_blank">Lihat File</a>
-                    </td>
-                    <td>
-                        <a href="{{ route('dokumens.edit', $dokumen) }}" class="btn btn-warning">Edit</a>
-                        <form action="{{ route('dokumens.destroy', $dokumen) }}" method="POST" style="display: inline-block;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Hapus</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+<h1>Daftar Dokumen</h1>
+<a href="{{ route('dokumen.create') }}" class="btn btn-primary">Unggah Dokumen</a>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Guru</th>
+            <th>Kriteria</th>
+            <th>File</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($dokumen as $item)
+        <tr>
+            <td>{{ $item->guru->nama }}</td>
+            <td>{{ $item->kriteria->nama }}</td>
+            <td><a href="{{ asset('storage/' . $item->file_path) }}" target="_blank">Lihat File</a></td>
+            <td>
+                <form action="{{ route('dokumen.destroy', $item->id) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger" onclick="return confirm('Yakin ingin menghapus?')">Hapus</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection
