4bf1729ebb698609e4e8c9e13614a905a13f1bb8
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2024-12-09 16:11:14 +0800

    memperbaiki isi view evaluasi

--- resources/views/evaluasi/index.blade.php
+++ resources/views/evaluasi/index.blade.php
@@ -1,30 +1,27 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <h1>Hasil Evaluasi Kinerja Guru</h1>
-    <table class="table mt-3">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Guru</th>
-                <th>Total Skor Diperoleh</th>
-                <th>Total Skor Maksimal</th>
-                <th>Persentase</th>
-            </tr>
-        </thead>
-        <tbody>
-            {{ dd($evaluasiResults) }}
-            @foreach ($evaluasiResults as $result)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $result['guru']->nama }}</td>
-                    <td>{{ $result['total_skor'] }}</td>
-                    <td>{{ $result['total_skor_maksimal'] }}</td>
-                    <td>{{ $result['persentase'] }}%</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+<h1>Evaluasi Kinerja Guru</h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Nama Guru</th>
+            <th>NIP</th>
+            <th>Email</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($guru as $item)
+        <tr>
+            <td>{{ $item->nama }}</td>
+            <td>{{ $item->nip }}</td>
+            <td>{{ $item->email }}</td>
+            <td>
+                <a href="{{ route('evaluasi.show', $item->id) }}" class="btn btn-primary">Evaluasi</a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection
