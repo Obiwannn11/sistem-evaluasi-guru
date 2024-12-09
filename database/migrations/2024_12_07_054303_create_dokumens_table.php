642a6e841178936e22db808ebf7b2bc1872119bc
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2024-12-09 15:37:24 +0800

    memperbaiki migration dokumen

--- database/migrations/2024_12_07_054303_create_dokumens_table.php
+++ database/migrations/2024_12_07_054303_create_dokumens_table.php
@@ -1,31 +1,23 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateDokumenTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        Schema::create('dokumens', function (Blueprint $table) {
-            $table->id(); // Primary key
-            $table->foreignId('id_guru')->constrained('gurus'); // Relasi ke guru
-            $table->foreignId('id_kriteria')->constrained('kriterias'); // Relasi ke kriteria
+        Schema::create('dokumen', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('guru_id')->constrained('guru')->onDelete('cascade');
+            $table->foreignId('kriteria_id')->constrained('kriteria')->onDelete('cascade');
             $table->string('file_path'); // Lokasi file
             $table->timestamps();
         });
-
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('dokumens');
+        Schema::dropIfExists('dokumen');
     }
-};
+}
