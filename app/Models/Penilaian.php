043e98d8838ac142a19093a2b15db49ea63a3ddf
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2024-12-09 15:46:40 +0800

    membuat model Penilaian

---
+++ app/Models/Penilaian.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Penilaian extends Model
+{
+    use HasFactory;
+
+    protected $table = 'penilaian';
+
+    protected $fillable = ['evaluasi_id', 'kriteria_id', 'nilai', 'komentar'];
+
+    public function evaluasi()
+    {
+        return $this->belongsTo(Evaluasi::class, 'evaluasi_id');
+    }
+
+    public function kriteria()
+    {
+        return $this->belongsTo(Kriteria::class, 'kriteria_id');
+    }
+}
