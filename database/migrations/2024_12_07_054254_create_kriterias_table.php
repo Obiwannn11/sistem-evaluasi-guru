3dd3ed6147a2b22025a13b3e814e30f1837976a4
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2024-12-09 19:39:43 +0800

    menambahkan kolom baru, dan nilai kriteria

--- database/migrations/2024_12_07_054254_create_kriterias_table.php
+++ database/migrations/2024_12_07_054254_create_kriterias_table.php
@@ -12,23 +12,25 @@ return new class extends Migration
             $table->id();
             $table->string('nama'); // Nama kriteria
             $table->enum('tipe', ['ordinal', 'numerik', 'persentase']); // Tipe nilai
+            $table->decimal('max_nilai', 8, 2); // Nilai maksimum kriteria
+            $table->decimal('bobot', 5, 2); // Bobot kriteria
             $table->timestamps();
         });
 
         // Insert predefined kriteria
         DB::table('kriterias')->insert([
-            ['nama' => 'Kalender Pendidikan', 'tipe' => 'ordinal'],
-            ['nama' => 'Program Tahunan', 'tipe' => 'ordinal'],
-            ['nama' => 'Program Semester', 'tipe' => 'ordinal'],
-            ['nama' => 'Silabus', 'tipe' => 'ordinal'],
-            ['nama' => 'Rencana Pelaksanaan Pembelajaran', 'tipe' => 'ordinal'],
-            ['nama' => 'Jadwal Tatap Muka', 'tipe' => 'ordinal'],
-            ['nama' => 'Agenda Harian', 'tipe' => 'ordinal'],
-            ['nama' => 'Daftar Nilai', 'tipe' => 'numerik'],
-            ['nama' => 'Kriteria Ketuntasan Minimal', 'tipe' => 'numerik'],
-            ['nama' => 'Absen Siswa', 'tipe' => 'persentase'],
-            ['nama' => 'Buku Pegangan Guru', 'tipe' => 'ordinal'],
-            ['nama' => 'Buku Pegangan Siswa', 'tipe' => 'ordinal'],
+            ['nama' => 'Kalender Pendidikan', 'tipe' => 'ordinal', 'max_nilai' => '2'],
+            ['nama' => 'Program Tahunan', 'tipe' => 'ordinal', 'max_nilai' => '2'],
+            ['nama' => 'Program Semester', 'tipe' => 'ordinal', 'max_nilai' => '2'],
+            ['nama' => 'Silabus', 'tipe' => 'ordinal', 'max_nilai' => '3'],
+            ['nama' => 'Rencana Pelaksanaan Pembelajaran', 'tipe' => 'ordinal', 'max_nilai' => '3'],
+            ['nama' => 'Jadwal Tatap Muka', 'tipe' => 'ordinal', 'max_nilai' => '1'],
+            ['nama' => 'Agenda Harian', 'tipe' => 'ordinal', 'max_nilai' => '2'],
+            ['nama' => 'Daftar Nilai', 'tipe' => 'numerik', 'max_nilai' => '100'],
+            ['nama' => 'Kriteria Ketuntasan Minimal', 'tipe' => 'numerik', 'max_nilai' => '100'],
+            ['nama' => 'Absen Siswa', 'tipe' => 'persentase', 'max_nilai' => '100'],
+            ['nama' => 'Buku Pegangan Guru', 'tipe' => 'ordinal', 'max_nilai' => '1'],
+            ['nama' => 'Buku Pegangan Siswa', 'tipe' => 'ordinal', 'max_nilai' => '1'],
         ]);
     }
 
