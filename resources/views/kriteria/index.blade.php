fafc4f8459e56d9a6d54cda4653658a426c256eb
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2024-12-09 16:05:51 +0800

    memperbaiki isi view kriteria

--- resources/views/kriteria/index.blade.php
+++ resources/views/kriteria/index.blade.php
@@ -1,35 +1,21 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <h1>Daftar Kriteria</h1>
-    <a href="{{ route('kriterias.create') }}" class="btn btn-primary">Tambah Kriteria</a>
-    <table class="table mt-3">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Nama Kriteria</th>
-                <th>Bobot</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($kriterias as $kriteria)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $kriteria->nama_kriteria }}</td>
-                    <td>{{ $kriteria->bobot }}</td>
-                    <td>
-                        <a href="{{ route('kriterias.edit', $kriteria->id) }}" class="btn btn-warning">Edit</a>
-                        <form action="{{ route('kriterias.destroy', $kriteria->id) }}" method="POST" style="display: inline-block;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Hapus</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+<h1>Daftar Kriteria</h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Nama Kriteria</th>
+            <th>Jenis Nilai</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($kriteria as $item)
+        <tr>
+            <td>{{ $item->nama }}</td>
+            <td>{{ $item->jenis_nilai }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection
