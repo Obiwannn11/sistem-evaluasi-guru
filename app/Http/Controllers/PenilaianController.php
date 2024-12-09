ae5835ddd28ca6889b0019134da8d1f70ecabad6
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2024-12-09 15:51:38 +0800

    membuat controller penilaian

---
+++ app/Http/Controllers/PenilaianController.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Evaluasi;
+use App\Models\Penilaian;
+use Illuminate\Http\Request;
+
+class PenilaianController extends Controller
+{
+    public function edit($id)
+    {
+        $evaluasi = Evaluasi::with('penilaian.kriteria')->findOrFail($id);
+        return view('penilaian.edit', compact('evaluasi'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $evaluasi = Evaluasi::findOrFail($id);
+
+        foreach ($request->input('penilaian', []) as $penilaianData) {
+            Penilaian::updateOrCreate(
+                ['evaluasi_id' => $id, 'kriteria_id' => $penilaianData['kriteria_id']],
+                ['nilai' => $penilaianData['nilai'], 'komentar' => $penilaianData['komentar']]
+            );
+        }
+
+        return redirect()->route('evaluasi.show', $id)->with('success', 'Penilaian berhasil disimpan.');
+    }
+}
