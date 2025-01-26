79c53ab78a470b962e62c77bc1c12e07d30cc0e9
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-01-26 20:14:19 +0800

    Mengomentari seeder karena mau disesuaikan dengan model Guru, dari sebelumnya menggunakan Model User

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Guru;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
@@ -13,20 +15,26 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Mustikasari',
-            'email' => 'admin@smkmadani.ac.id',
-            'password' => bcrypt('password'),
-            'role' => 'superguru', // Atau 'superguru'
-            'foto' => 'noprofil.png'
-        ]);
+        // Guru::create([
+        //     'nama' => 'Mustikasari',
+        //     'nip' => '0192832',
+        //     'email' => 'admin@smk.id',
+        //     'telepon' => '0812398212',
+        //     'is_admin' => true,
+        //     'password' => Hash::make('password'),
+        // ]);
 
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin123@smkmadani.ac.id',
-            'password' => bcrypt('admin123'),
-            'role' => 'superguru', // Atau 'superguru'
-            'foto' => 'noprofil.png'
-        ]);
+        // User::create([
+        //     'name' => 'RizkaMalyah',
+        //     'email' => 'rizka@smk.id',
+        //     'password' => Hash::make('password'),
+        // ]);
+
+        // User::create([
+        //     'name' => 'RizkaMalyah',
+        //     'email' => 'rizka@smk.id',
+        //     'password' => Hash::make('password'),
+        // ]);
+
     }
 }
