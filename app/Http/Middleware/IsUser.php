6d6df7a5d9c02e4ad7f3347c42fde36a29ab6d1d
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-01-28 04:52:44 +0800

    membuat Middleware untuk mengecek apakah merupakan login sebagai User/Guru atau sebagai Admin/KepalaSekolah

---
+++ app/Http/Middleware/IsUser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Periksa apakah pengguna adalah admin
+        if (Auth::User()->is_admin == false) {
+            return $next($request);
+        }
+
+        // Jika bukan admin, redirect atau lakukan tindakan lainnya
+        return redirect(route('dashboard'));
+    }
+}
