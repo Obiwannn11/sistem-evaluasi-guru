d08c223b83ac3f1141ad00a449e4b327dfb33a10
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-07 22:47:21 +0800

    memperbaiki view index user guru

--- resources/views/user/guru/index.blade.php
+++ resources/views/user/guru/index.blade.php
@@ -2,29 +2,41 @@
 
 @section('content')
 <h1>Data Guru</h1>
-<form action="{{ route('user.guru.update', $data['id']) }}" method="POST">
+
+<!-- Form untuk mengedit data guru -->
+<form >
     @csrf
-    @method('PUT')
+    @method('PUT') <!-- Menggunakan metode PUT untuk update -->
     <div class="mb-3 col-md-5">
-        <label for="nama" class="form-label">ID</label>
-        <input type="text" name="nama" value="{{ $data['id'] }}" class="form-control" required>
+        <label for="id" class="form-label">ID</label>
+        <input type="text" name="id" value="{{ $data['id'] }}" class="form-control" disabled required>
     </div>
+
     <div class="mb-3 col-md-5">
         <label for="nama" class="form-label">Nama</label>
         <input type="text" name="nama" value="{{ $data['nama'] }}" class="form-control" required>
     </div>
+
     <div class="mb-3 col-md-5">
         <label for="nip" class="form-label">NIP</label>
         <input type="text" name="nip" value="{{ $data['nip'] }}" class="form-control" required>
     </div>
+
     <div class="mb-3 col-md-5">
         <label for="email" class="form-label">Email</label>
         <input type="email" name="email" value="{{ $data['email'] }}" class="form-control" required>
     </div>
+
     <div class="mb-3 col-md-5">
         <label for="telepon" class="form-label">Telepon</label>
         <input type="text" name="telepon" value="{{ $data['telepon'] }}" class="form-control" required>
     </div>
-    <button type="submit" class="btn btn-primary">Simpan</button>
+
+    <div class="d-flex justify-content-between mb-3 col-md-5">
+
+        {{-- <a href="{{ route('user.guru.index') }}" class="btn btn-primary">Kembali</a> --}}
+        <a href="{{ route('user.guru.edit', $data['id']) }}" class="btn btn-warning">Edit Data</a>
+    </div>
 </form>
+
 @endsection
