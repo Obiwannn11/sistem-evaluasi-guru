0ea48fd770756536c1de2eb9fb9387d99aaef7ab
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-07 22:46:44 +0800

    membuat view edit beserta logic form nya

--- resources/views/user/guru/edit.blade.php
+++ resources/views/user/guru/edit.blade.php
@@ -1,26 +1,33 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>Edit Guru</h1>
-<form action="{{ route('guru.update', $guru->id) }}" method="POST">
+<h1>Edit Data Guru ({{ Auth::user()->nama }})</h1>
+<form action="{{ route('user.guru.update', $data['id']) }}" method="POST">
     @csrf
     @method('PUT')
-    <div class="mb-3">
+    <div class="mb-3 col-md-5">
         <label for="nama" class="form-label">Nama</label>
-        <input type="text" name="nama" value="{{ $guru->nama }}" class="form-control" required>
+        <input type="text" name="nama" value="{{ $data['nama'] }}" class="form-control" required>
     </div>
-    <div class="mb-3">
+    <div class="mb-3 col-md-5">
         <label for="nip" class="form-label">NIP</label>
-        <input type="text" name="nip" value="{{ $guru->nip }}" class="form-control" required>
+        <input type="text" name="nip" value="{{ $data['nip'] }}" class="form-control" required>
     </div>
-    <div class="mb-3">
+    <div class="mb-3 col-md-5">
         <label for="email" class="form-label">Email</label>
-        <input type="email" name="email" value="{{ $guru->email }}" class="form-control" required>
+        <input type="email" name="email" value="{{ $data['email'] }}" class="form-control" required>
     </div>
-    <div class="mb-3">
+    <div class="mb-3 col-md-5">
         <label for="telepon" class="form-label">Telepon</label>
-        <input type="text" name="telepon" value="{{ $guru->telepon }}" class="form-control" required>
+        <input type="text" name="telepon" value="{{ $data['telepon'] }}" class="form-control" required>
     </div>
-    <button type="submit" class="btn btn-primary">Simpan</button>
+    <div class="mb-3 col-md-5">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" name="password" value="" class="form-control">
+    </div>
+    <div class="mb-3 col-md-5 d-flex justify-content-between">
+        <a href="{{ route('user.guru.index') }}" class="btn btn-primary">Kembali</a>
+        <button type="submit" class="btn btn-success">Simpan</button>
+    </div>
 </form>
 @endsection
