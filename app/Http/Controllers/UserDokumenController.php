db1ad5ba6e4c870adbde19034b027514d23ad226
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-08 01:12:39 +0800

    membuat fungsi store dan create

--- app/Http/Controllers/UserDokumenController.php
+++ app/Http/Controllers/UserDokumenController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Guru;
 use App\Models\Dokumen;
+use App\Models\Kriteria;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -32,7 +34,10 @@ class UserDokumenController extends Controller
      */
     public function create()
     {
-        //
+        $user = Auth::user()->id;
+        $guru = Guru::find($user);
+        $kriteria = Kriteria::all();
+        return view('user.dokumen.create', compact('guru', 'kriteria'));
     }
 
     /**
@@ -40,7 +45,25 @@ class UserDokumenController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request);
+        $validated = $request->validate([
+            'guru_id' => 'required|exists:gurus,id',
+            'kriteria_id' => 'required|exists:kriterias,id',
+            'file_path' => 'required|file|mimes:pdf,doc,docx|max:5210',
+        ]);
+
+        dd($validated);
+        // mengatur penyimpanan file
+        // variabel = kolom file_path -> di masukkan ke dalam path storage/app/public/dokumen di dalam disk public
+        $filePath = $request->file('file_path')->store('dokumen', 'public');
+
+        // $validated['file_path'] = basename($filePath); // digunakan jika hanya simpan nama file tanpa nama path di database
+        $validated['file_path'] = $filePath; // digunakan jika simpan nama beserta path di database
+
+        Dokumen::create($validated);
+
+        return redirect()->route('dokumen.index')->with('success', 'Dokumen berhasil diunggah.');
+
     }
 
     /**
