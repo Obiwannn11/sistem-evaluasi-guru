5d2b27f154367cfed249bd486ede7c00ad34435c
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-08 16:37:25 +0800

    memperbaiki tulisan menu sidebar kepala seklah

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -71,7 +71,7 @@
         {{-- nav item evaluasi --}}
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('evaluasi.index') }}" <i class="bi bi-grid"></i>
-                <i class="bi bi-clipboard-check"></i><span>Berikan Evaluasi</span>
+                <i class="bi bi-clipboard-check"></i><span>Berikan Penilaian</span>
             </a>
         </li>
         {{-- end of nav item evaluasi --}}
