e6341f66a00937e78ed57ea8ccb447d4ccf7826b
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-08 14:45:20 +0800

    memperbaiki href action button, view file, edit file dan delete file user/dokumen

--- resources/views/user/dokumen/index.blade.php
+++ resources/views/user/dokumen/index.blade.php
@@ -3,6 +3,20 @@
 @section('content')
 <h1>Dokumen Anda ({{ Auth::User()->nama }})</h1>
 <a href="{{ route('user.dokumen.create') }}" class="mb-3 btn btn-primary">Unggah Dokumen Baru</a>
+
+
+{{-- Area untuk menampilkan pesan error validasi --}}
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+
 <table class="table">
     <thead>
         <tr>
@@ -19,13 +33,13 @@
         <tr>
             <td>{{ $item->guru->nama }}</td>
             <td>{{ $item->kriteria->nama }}</td>
-            <td><a href="{{ asset('storage/' . $item->file_path) }}" target="_blank">Lihat File</a></td>
+            <td><a href="{{ asset('storage/' . $item->file_path) }}" target="_blank" class="btn btn-success" > <i class="bi bi-file-earmark-text"></i> Lihat File</a></td>
             <td>
                 <a href="{{ route('guru.edit', $item->id) }}" class="btn btn-warning">Edit</a>
-                <form action="{{ route('dokumen.destroy', $item->id) }}" method="POST" style="display:inline;">
+                <form action="{{ route('user.dokumen.destroy', $item->id) }}" method="POST" style="display:inline;">
                     @csrf
                     @method('DELETE')
-                    <button class="btn btn-danger" onclick="return confirm('Yakin ingin menghapus?')">Hapus</button>
+                    <button class="btn btn-danger" ">Hapus</button>
                 </form>
             </td>
         </tr>
