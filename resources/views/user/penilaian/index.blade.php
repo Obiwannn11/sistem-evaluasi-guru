5ccad6a98f3c03bd4fcf28aacfa8f1f806f81477
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-08 15:12:37 +0800

    memperbaiki tabel informasi dan mengeluarkan nilai akhir

--- resources/views/user/penilaian/index.blade.php
+++ resources/views/user/penilaian/index.blade.php
@@ -2,7 +2,27 @@
 
 @section('content')
 <h1>Detail Penilaian Guru: {{ Auth::User()->nama }}</h1>
-<p>NIP: {{ Auth::User()->nip }}</p>
+{{-- <p>NIP: {{ Auth::User()->nip }}</p>
+<p>Nilai Akhir: {{ $totalNilai }} / 100</p> --}}
+
+<div class="mb-3 col-md-5">
+    <table class="table table-md table-bordered ">
+        <tbody>
+            <tr>
+                <th scope="row">Nama Guru</th>
+                <td>: {{ Auth::User()->nama }}</td>
+            </tr>
+            <tr>
+                <th scope="row">NIP</th>
+                <td>: {{ Auth::User()->nip }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Nilai Akhir</th>
+                <td>: {{ $totalNilai }} / 100</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 <table class="table">
     <thead>
@@ -15,12 +35,16 @@
     <tbody>
         @foreach ($evaluasi as $e)
         <tr>
-            <td>{{ $e->kriteria->nama }}</td> <!-- Asumsi ada relasi kriteria di model Evaluasi -->
+            <td>{{ $e->kriteria->nama }}</td> <!--  ada relasi kriteria di model Evaluasi -->
             <td>{{ $e->nilai ?? 0 }}</td>
             <td>{{ $e->komentar ?? 'Tidak ada Komentar'}}</td>
         </tr>
         @endforeach
     </tbody>
 </table>
+<div class="mb-3 col-md-5">
+    <label for="guru_id" class="form-label"><strong>Nilai Akhir Kamu ({{ Auth::User()->nama }})</strong></label>
+    <input type="text" id="guru_id" class="form-control" name="guru_id" value="{{ $totalNilai }}" disabled>
+</div>
 <a href="" class="btn btn-success"><i class="bi bi-printer"></i> Cetak Nilai</a>
 @endsection
